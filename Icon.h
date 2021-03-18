cf79fc79b4138816dd41fab9fdd4a5d4d5da7c98
knoxfighter <knoxfighter@asdff45.de>
2021-03-18 23:02:39 +0100

    deleted copy/move for Icons

--- Icon.h
+++ Icon.h
@@ -6,8 +6,15 @@
 class Icon {
 public:
 	IDirect3DTexture9* texture;
+	Icon() = delete;
 	Icon(UINT name, HMODULE dll, IDirect3DDevice9* d3d9Device);
 	~Icon();
+
+	// delete copy/move
+	Icon(const Icon& other) = delete;
+	Icon(Icon&& other) noexcept = delete;
+	Icon& operator=(const Icon& other) = delete;
+	Icon& operator=(Icon&& other) noexcept = delete;
 	
 private:
 	UINT width;
