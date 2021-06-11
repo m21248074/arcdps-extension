fdd419daf2068c77f40e819d44d6971cdef59a50
knoxfighter <knoxfighter@asdff45.de>
2021-06-11 21:27:09 +0200

    updated map.h

--- map.h
+++ map.h
@@ -37,7 +37,7 @@
 #define EVAL5(...) EVAL4(EVAL4(EVAL4(__VA_ARGS__)))
 
 #ifdef _MSC_VER
-// MSVC needs more evaluations
+ // MSVC needs more evaluations
 #define EVAL6(...) EVAL5(EVAL5(EVAL5(__VA_ARGS__)))
 #define EVAL(...)  EVAL6(EVAL6(__VA_ARGS__))
 #else
@@ -66,6 +66,8 @@
 
 #define MAP0_UD(f, userdata, x, peek, ...) f(x,userdata) DEFER ( MAP_NEXT(peek, MAP1_UD) ) ( f, userdata, peek, __VA_ARGS__ ) 
 #define MAP1_UD(f, userdata, x, peek, ...) f(x,userdata) DEFER ( MAP_NEXT(peek, MAP0_UD) ) ( f, userdata, peek, __VA_ARGS__ ) 
+#define MAP0_UD2(f, userdata, x, y, peek, ...) f(x,y,userdata) DEFER ( MAP_NEXT(peek, MAP1_UD2) ) ( f, userdata, peek, __VA_ARGS__ ) 
+#define MAP1_UD2(f, userdata, x, y, peek, ...) f(x,y,userdata) DEFER ( MAP_NEXT(peek, MAP0_UD2) ) ( f, userdata, peek, __VA_ARGS__ ) 
 
 #define MAP0_UD_I(f, userdata, index, x, peek, ...) f(x,userdata,index) DEFER ( MAP_NEXT(peek, MAP1_UD_I) ) ( f, userdata, MAP_INC(index), peek, __VA_ARGS__ ) 
 #define MAP1_UD_I(f, userdata, index, x, peek, ...) f(x,userdata,index) DEFER ( MAP_NEXT(peek, MAP0_UD_I) ) ( f, userdata, MAP_INC(index), peek, __VA_ARGS__ ) 
@@ -86,55 +88,56 @@
 #define MAP_LIST1_UD_I(f, userdata, index, x, peek, ...) , f(x, userdata, index) DEFER ( MAP_NEXT(peek, MAP_LIST0_UD_I) ) ( f, userdata, MAP_INC(index), peek, __VA_ARGS__ ) 
 #define MAP_LIST2_UD_I(f, userdata, index, x, peek, ...)   f(x, userdata, index) DEFER ( MAP_NEXT(peek, MAP_LIST0_UD_I) ) ( f, userdata, MAP_INC(index), peek, __VA_ARGS__ )
 
-/**
- * Applies the function macro `f` to each of the remaining parameters.
- */
+ /**
+  * Applies the function macro `f` to each of the remaining parameters.
+  */
 #define MAP(f, ...) EVAL(MAP1(f, __VA_ARGS__, ()()(), ()()(), ()()(), 0))
 #define MAP2(f, ...) EVAL(MAP12(f, __VA_ARGS__, ()()(), ()()(), ()()(), 0))
 
-/**
- * Applies the function macro `f` to each of the remaining parameters and
- * inserts commas between the results.
- */
+  /**
+   * Applies the function macro `f` to each of the remaining parameters and
+   * inserts commas between the results.
+   */
 #define MAP_LIST(f, ...) EVAL(MAP_LIST2(f, __VA_ARGS__, ()()(), ()()(), ()()(), 0))
 
-/**
- * Applies the function macro `f` to each of the remaining parameters and passes userdata as the second parameter to each invocation,
- * e.g. MAP_UD(f, x, a, b, c) evaluates to f(a, x) f(b, x) f(c, x)
- */
+   /**
+	* Applies the function macro `f` to each of the remaining parameters and passes userdata as the second parameter to each invocation,
+	* e.g. MAP_UD(f, x, a, b, c) evaluates to f(a, x) f(b, x) f(c, x)
+	*/
 #define MAP_UD(f, userdata, ...) EVAL(MAP1_UD(f, userdata, __VA_ARGS__, ()()(), ()()(), ()()(), 0))
+#define MAP_UD2(f, userdata, ...) EVAL(MAP1_UD2(f, userdata, __VA_ARGS__, ()()(), ()()(), ()()(), 0))
 
-/**
- * Applies the function macro `f` to each of the remaining parameters, inserts commas between the results,
- * and passes userdata as the second parameter to each invocation,
- * e.g. MAP_LIST_UD(f, x, a, b, c) evaluates to f(a, x), f(b, x), f(c, x)
- */
+	/**
+	 * Applies the function macro `f` to each of the remaining parameters, inserts commas between the results,
+	 * and passes userdata as the second parameter to each invocation,
+	 * e.g. MAP_LIST_UD(f, x, a, b, c) evaluates to f(a, x), f(b, x), f(c, x)
+	 */
 #define MAP_LIST_UD(f, userdata, ...) EVAL(MAP_LIST2_UD(f, userdata, __VA_ARGS__, ()()(), ()()(), ()()(), 0))
 #define MAP_LIST_UD2(f, userdata, ...) EVAL(MAP_LIST2_UD2(f, userdata, __VA_ARGS__, ()()(), ()()(), ()()(), 0))
 
-/**
- * Applies the function macro `f` to each of the remaining parameters, passes userdata as the second parameter to each invocation,
- * and the index of the invocation as the third parameter,
- * e.g. MAP_UD_I(f, x, a, b, c) evaluates to f(a, x, 0) f(b, x, 1) f(c, x, 2)
- */
+	 /**
+	  * Applies the function macro `f` to each of the remaining parameters, passes userdata as the second parameter to each invocation,
+	  * and the index of the invocation as the third parameter,
+	  * e.g. MAP_UD_I(f, x, a, b, c) evaluates to f(a, x, 0) f(b, x, 1) f(c, x, 2)
+	  */
 #define MAP_UD_I(f, userdata, ...) EVAL(MAP1_UD_I(f, userdata, 0, __VA_ARGS__, ()()(), ()()(), ()()(), 0))
 
-/**
- * Applies the function macro `f` to each of the remaining parameters, inserts commas between the results,
- * passes userdata as the second parameter to each invocation, and the index of the invocation as the third parameter,
- * e.g. MAP_LIST_UD_I(f, x, a, b, c) evaluates to f(a, x, 0), f(b, x, 1), f(c, x, 2)
- */
+	  /**
+	   * Applies the function macro `f` to each of the remaining parameters, inserts commas between the results,
+	   * passes userdata as the second parameter to each invocation, and the index of the invocation as the third parameter,
+	   * e.g. MAP_LIST_UD_I(f, x, a, b, c) evaluates to f(a, x, 0), f(b, x, 1), f(c, x, 2)
+	   */
 #define MAP_LIST_UD_I(f, userdata, ...) EVAL(MAP_LIST2_UD_I(f, userdata, 0, __VA_ARGS__, ()()(), ()()(), ()()(), 0))
 
-/*
- * Because the preprocessor can't do arithmetic that produces integer literals for the *_I macros, we have to do it manually.
- * Since the number of parameters is limited anyways, this is sufficient for all cases. If extra EVAL layers are added, these
- * definitions have to be extended. This is equivalent to the way Boost.preprocessor does it:
- * https://github.com/boostorg/preprocessor/blob/develop/include/boost/preprocessor/arithmetic/inc.hpp
- * The *_I macros could alternatively pass C expressions such as (0), (0+1), (0+1+1...) to the user macro, but passing 0, 1, 2 ...
- * allows the user to incorporate the index into C identifiers, e.g. to define a function like test_##index () for each
- * macro invocation.
- */
+	   /*
+		* Because the preprocessor can't do arithmetic that produces integer literals for the *_I macros, we have to do it manually.
+		* Since the number of parameters is limited anyways, this is sufficient for all cases. If extra EVAL layers are added, these
+		* definitions have to be extended. This is equivalent to the way Boost.preprocessor does it:
+		* https://github.com/boostorg/preprocessor/blob/develop/include/boost/preprocessor/arithmetic/inc.hpp
+		* The *_I macros could alternatively pass C expressions such as (0), (0+1), (0+1+1...) to the user macro, but passing 0, 1, 2 ...
+		* allows the user to incorporate the index into C identifiers, e.g. to define a function like test_##index () for each
+		* macro invocation.
+		*/
 #define MAP_INC_0 1
 #define MAP_INC_1 2
 #define MAP_INC_2 3
