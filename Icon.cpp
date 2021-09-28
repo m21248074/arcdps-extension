3b0816bfe968af5569ff3b83559b8e2f74c95e35
knox <knoxfighter@asdff45.de>
2021-09-28 02:26:05 +0200

    added print macro to Icon.cpp, when externally defined, this allows tracking prints

--- Icon.cpp
+++ Icon.cpp
@@ -5,18 +5,29 @@
 #include <thread>
 #include <wincodec.h>
 
+#ifndef PRINT_LINE
+#define PRINT_LINE()
+#endif
+
 IconLoader iconLoader;
 
 Icon::Icon(UINT name, HMODULE dll, IDirect3DDevice9* d3d9Device, ID3D11Device* d3d11Device) {
+	PRINT_LINE()
+
 	std::string nameString(std::to_string(name));
+
+	PRINT_LINE()
 
 	HRSRC imageResHandle = FindResource(dll, MAKEINTRESOURCE(name), L"PNG");
 	if (!imageResHandle) {
 		// not found
 		std::string text = "Error finding Resource: ";
 		text.append(nameString);
-		throw std::runtime_error(text);
+		return;
+		// throw std::runtime_error(text);
 	}
+
+	PRINT_LINE()
 
 	// does not need to be freed
 	HGLOBAL imageResDataHandle = LoadResource(dll, imageResHandle);
@@ -24,24 +35,33 @@ Icon::Icon(UINT name, HMODULE dll, IDirect3DDevice9* d3d9Device, ID3D11Device* d
 		// loading failed
 		std::string text = "Error loading resource: ";
 		text.append(nameString);
-		throw std::runtime_error(text);
+		return;
+		// throw std::runtime_error(text);
 	}
+
+	PRINT_LINE()
 
 	LPVOID imageFile = LockResource(imageResDataHandle);
 	if (!imageFile) {
 		// locking failed
 		std::string text = "Error locking resource: ";
 		text.append(nameString);
-		throw std::runtime_error(text);
+		return;
+		// throw std::runtime_error(text);
 	}
+
+	PRINT_LINE()
 
 	DWORD imageFileSize = SizeofResource(dll, imageResHandle);
 	if (!imageFileSize) {
 		// error getting size of file
 		std::string text = "Error getting Size of Resource: ";
 		text.append(nameString);
-		throw std::runtime_error(text);
+		return;
+		// throw std::runtime_error(text);
 	}
+
+	PRINT_LINE()
 
 	HRESULT coInitializeResult = CoInitialize(NULL);
 	if (!SUCCEEDED(coInitializeResult)) {
@@ -50,8 +70,11 @@ Icon::Icon(UINT name, HMODULE dll, IDirect3DDevice9* d3d9Device, ID3D11Device* d
 		text.append(nameString);
 		text.append(" - ");
 		text.append(std::to_string(coInitializeResult));
-		throw std::runtime_error(text);
+		return;
+		// throw std::runtime_error(text);
 	}
+
+	PRINT_LINE()
 
 	IWICImagingFactory* m_pIWICFactory = NULL;
 	HRESULT createInstance = CoCreateInstance(CLSID_WICImagingFactory, NULL, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&m_pIWICFactory));
@@ -61,8 +84,11 @@ Icon::Icon(UINT name, HMODULE dll, IDirect3DDevice9* d3d9Device, ID3D11Device* d
 		text.append(nameString);
 		text.append(" - ");
 		text.append(std::to_string(createInstance));
-		throw std::runtime_error(text);
+		return;
+		// throw std::runtime_error(text);
 	}
+
+	PRINT_LINE()
 
 	IWICStream* pIWICStream = NULL;
 	HRESULT streamRes = m_pIWICFactory->CreateStream(&pIWICStream);
@@ -72,8 +98,11 @@ Icon::Icon(UINT name, HMODULE dll, IDirect3DDevice9* d3d9Device, ID3D11Device* d
 		text.append(nameString);
 		text.append(" - ");
 		text.append(std::to_string(streamRes));
-		throw std::runtime_error(text);
+		return;
+		// throw std::runtime_error(text);
 	}
+
+	PRINT_LINE()
 
 	HRESULT initializeFromMemoryRes = pIWICStream->InitializeFromMemory(reinterpret_cast<BYTE*>(imageFile), imageFileSize);
 	if (!SUCCEEDED(initializeFromMemoryRes)) {
@@ -82,8 +111,11 @@ Icon::Icon(UINT name, HMODULE dll, IDirect3DDevice9* d3d9Device, ID3D11Device* d
 		text.append(nameString);
 		text.append(" - ");
 		text.append(std::to_string(initializeFromMemoryRes));
-		throw std::runtime_error(text);
+		return;
+		// throw std::runtime_error(text);
 	}
+
+	PRINT_LINE()
 
 	IWICBitmapDecoder* pIDecoder = NULL;
 	HRESULT decoderFromStreamRes = m_pIWICFactory->CreateDecoderFromStream(pIWICStream, NULL, WICDecodeMetadataCacheOnLoad, &pIDecoder);
@@ -93,8 +125,11 @@ Icon::Icon(UINT name, HMODULE dll, IDirect3DDevice9* d3d9Device, ID3D11Device* d
 		text.append(nameString);
 		text.append(" - ");
 		text.append(std::to_string(decoderFromStreamRes));
-		throw std::runtime_error(text);
+		return;
+		// throw std::runtime_error(text);
 	}
+
+	PRINT_LINE()
 
 	IWICBitmapFrameDecode* pIDecodeFrame = NULL;
 	HRESULT getFrameRes = pIDecoder->GetFrame(0, &pIDecodeFrame);
@@ -104,8 +139,11 @@ Icon::Icon(UINT name, HMODULE dll, IDirect3DDevice9* d3d9Device, ID3D11Device* d
 		text.append(nameString);
 		text.append(" - ");
 		text.append(std::to_string(getFrameRes));
-		throw std::runtime_error(text);
+		return;
+		// throw std::runtime_error(text);
 	}
+
+	PRINT_LINE()
 
 	WICPixelFormatGUID pixelFormat;
 	HRESULT pixelFormatRes = pIDecodeFrame->GetPixelFormat(&pixelFormat);
@@ -116,7 +154,10 @@ Icon::Icon(UINT name, HMODULE dll, IDirect3DDevice9* d3d9Device, ID3D11Device* d
 		text.append(" - ");
 		text.append(std::to_string(pixelFormatRes));
 		return;
+		// throw std::runtime_error(text);
 	}
+
+	PRINT_LINE()
 
 	IWICComponentInfo* pIComponentInfo = NULL;
 	HRESULT componentInfoRes = m_pIWICFactory->CreateComponentInfo(pixelFormat, &pIComponentInfo);
@@ -127,7 +168,10 @@ Icon::Icon(UINT name, HMODULE dll, IDirect3DDevice9* d3d9Device, ID3D11Device* d
 		text.append(" - ");
 		text.append(std::to_string(componentInfoRes));
 		return;
+		// throw std::runtime_error(text);
 	}
+
+	PRINT_LINE()
 
 	IWICPixelFormatInfo* pIPixelFormatInfo;
 	HRESULT pixelFormatInfoRes = pIComponentInfo->QueryInterface(__uuidof(IWICPixelFormatInfo), reinterpret_cast<void**>(&pIPixelFormatInfo));
@@ -138,7 +182,10 @@ Icon::Icon(UINT name, HMODULE dll, IDirect3DDevice9* d3d9Device, ID3D11Device* d
 		text.append(" - ");
 		text.append(std::to_string(pixelFormatInfoRes));
 		return;
+		// throw std::runtime_error(text);
 	}
+
+	PRINT_LINE()
 
 	UINT bitsPerPixel;
 	HRESULT bitsPerPixelRes = pIPixelFormatInfo->GetBitsPerPixel(&bitsPerPixel);
@@ -149,7 +196,10 @@ Icon::Icon(UINT name, HMODULE dll, IDirect3DDevice9* d3d9Device, ID3D11Device* d
 		text.append(" - ");
 		text.append(std::to_string(bitsPerPixelRes));
 		return;
+		// throw std::runtime_error(text);
 	}
+
+	PRINT_LINE()
 
 	pIDecodeFrame->GetSize(&width, &height);
 
@@ -166,13 +216,17 @@ Icon::Icon(UINT name, HMODULE dll, IDirect3DDevice9* d3d9Device, ID3D11Device* d
 		text.append(std::to_string(copyPixelsRes));
 		delete[] pixelBuffer;
 		return;
+		// throw std::runtime_error(text);
 	}
+
+	PRINT_LINE()
 
 	// Create texture
 	if (d3d11Device) {
+		PRINT_LINE()
+		
 		D3D11_TEXTURE2D_DESC desc;
-		ZeroMemory(&desc, sizeof(desc));
-		desc.Width = width;
+		ZeroMemory(&desc, sizeof desc);
 		desc.Width = width;
 		desc.Height = height;
 		desc.MipLevels = 1;
@@ -181,23 +235,33 @@ Icon::Icon(UINT name, HMODULE dll, IDirect3DDevice9* d3d9Device, ID3D11Device* d
 		desc.SampleDesc.Count = 1;
 		desc.Usage = D3D11_USAGE_DEFAULT;
 		desc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
-		desc.CPUAccessFlags = 0;
-
-		ID3D11Texture2D* pTexture = NULL;
+		
+		PRINT_LINE()
+		
 		D3D11_SUBRESOURCE_DATA subResource;
+		ZeroMemory(&subResource, sizeof subResource);
 		subResource.pSysMem = pixelBuffer;
 		subResource.SysMemPitch = desc.Width * 4;
 		subResource.SysMemSlicePitch = 0;
+		
+		PRINT_LINE()
+		
+		ID3D11Texture2D* pTexture = nullptr;
 		HRESULT createTexture2DRes = d3d11Device->CreateTexture2D(&desc, &subResource, &pTexture);
 		if (!SUCCEEDED(createTexture2DRes)) {
+			pTexture->Release();
 			// error copying pixels to buffer
 			std::string text = "Error creating 2d texture: ";
 			text.append(nameString);
 			text.append(" - ");
 			text.append(std::to_string(createTexture2DRes));
+			PRINT_LINE()
 			return;
+			// throw std::runtime_error(text);
 		}
-
+		
+		PRINT_LINE()
+		
 		// Create texture view
 		D3D11_SHADER_RESOURCE_VIEW_DESC srvDesc;
 		ZeroMemory(&srvDesc, sizeof(srvDesc));
@@ -205,9 +269,27 @@ Icon::Icon(UINT name, HMODULE dll, IDirect3DDevice9* d3d9Device, ID3D11Device* d
 		srvDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
 		srvDesc.Texture2D.MipLevels = desc.MipLevels;
 		srvDesc.Texture2D.MostDetailedMip = 0;
+		
+		PRINT_LINE()
+		
 		d3d11Device->CreateShaderResourceView(pTexture, &srvDesc, &d11texture);
+		if (!SUCCEEDED(createTexture2DRes)) {
+			// error copying pixels to buffer
+			std::string text = "Error creating shader Resource View: ";
+			text.append(nameString);
+			text.append(" - ");
+			text.append(std::to_string(createTexture2DRes));
+			PRINT_LINE()
+			return;
+		}
+		PRINT_LINE()
+		
 		pTexture->Release();
+		
+		PRINT_LINE()
 	} else if (d3d9Device) {
+		PRINT_LINE()
+
 		HRESULT createTextureRes = d3d9Device->CreateTexture(width, height, 1, 0, D3DFMT_A8R8G8B8, D3DPOOL_MANAGED, &d9texture, NULL);
 		if (!SUCCEEDED(createTextureRes)) {
 			// error creating d3d9 texture
@@ -219,12 +301,16 @@ Icon::Icon(UINT name, HMODULE dll, IDirect3DDevice9* d3d9Device, ID3D11Device* d
 			return;
 		}
 
+		PRINT_LINE()
+
 		D3DLOCKED_RECT rect;
 		d9texture->LockRect(0, &rect, 0, D3DLOCK_DISCARD);
 		unsigned char* dest = static_cast<unsigned char*>(rect.pBits);
 		memcpy(dest, pixelBuffer, sizeof(char) * width * height * 4);
 		d9texture->UnlockRect(0);
 	}
+
+	PRINT_LINE()
 
 	// cleanup / this will not be called when function is failing, which results in a small leak
 	delete[] pixelBuffer;
@@ -234,13 +320,17 @@ Icon::Icon(UINT name, HMODULE dll, IDirect3DDevice9* d3d9Device, ID3D11Device* d
 	pIDecodeFrame->Release();
 	pIComponentInfo->Release();
 	pIPixelFormatInfo->Release();
+
+	PRINT_LINE()
 }
 
 Icon::~Icon() {
+	PRINT_LINE()
 	if (d9texture)
 		d9texture->Release();
 	if (d11texture)
 		d11texture->Release();
+	PRINT_LINE()
 }
 
 void IconLoader::Setup(HMODULE new_dll, IDirect3DDevice9* new_d3d9Device, ID3D11Device* new_d3d11device) {
@@ -250,6 +340,12 @@ void IconLoader::Setup(HMODULE new_dll, IDirect3DDevice9* new_d3d9Device, ID3D11
 }
 
 void* IconLoader::getTexture(UINT name) {
+	PRINT_LINE()
+
+	if (!d3d9Device && !d3d11device) {
+		return nullptr;
+	}
+
 	auto texture = textures.find(name);
 	if (texture != textures.end()) {
 		const auto& tex = texture->second;
@@ -261,12 +357,13 @@ void* IconLoader::getTexture(UINT name) {
 		}
 	}
 
-	queueMutex.lock();
-	if (std::ranges::find(queue, name) == queue.end()) {
-		queue.push_back(name);
+	{
+		std::lock_guard<std::mutex> guard(queueMutex);
+		if (std::ranges::find(queue, name) == queue.end()) {
+			queue.push_back(name);
+		}
 	}
-	queueMutex.unlock();
-
+	
 	bool expected = false;
 	if (thread_running.compare_exchange_strong(expected, true)) {
 		std::thread t(&IconLoader::thread_fun, this);
@@ -277,7 +374,9 @@ void* IconLoader::getTexture(UINT name) {
 }
 
 void IconLoader::thread_fun() {
+	PRINT_LINE()
 	while (!queue.empty()) {
+		PRINT_LINE()
 		queueMutex.lock();
 		UINT name = queue.back();
 		queue.pop_back();
@@ -288,8 +387,12 @@ void IconLoader::thread_fun() {
 			continue;
 		}
 
+		PRINT_LINE()
 		// load texture (texture is loaded in Icon constructor)
 		textures.try_emplace(name, name, dll, d3d9Device, d3d11device);
+		PRINT_LINE()
 	}
+	PRINT_LINE()
 	thread_running = false;
+	PRINT_LINE()
 }
