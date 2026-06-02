53077244297c3f410f8da195c64f23eac8457178
knox <knoxfighter@asdff45.de>
2026-06-02 22:59:06 +0200

    add github action to automatically update vcpkg-registry

--- updateVersion.ps1
+++
@@ -1,9 +0,0 @@
-$file = $args[0]
-$regex = '(.*(?:(?:FILEVERSION)|(?:FileVersion)|(?:PRODUCTVERSION)|(?:ProductVersion)).*?(?:\d*[.,]){3})(\d*)(.*)'
-
-$tmp = Get-Content $file | Foreach {
-        [regex]::replace($_, $regex, 
-            {param($m) $m.Groups[1].Value + ([int]$m.Groups[2].Value + 1) + $m.Groups[3].Value})
-    }
-
-$tmp | Set-Content $file
